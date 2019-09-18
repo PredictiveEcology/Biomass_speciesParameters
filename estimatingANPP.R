18341c302c8b6c247fdc4f07ee32ccbf9a67326e
Ian <ianmseddy@gmail.com>
2019-09-18 13:50:49 -0700

    turned into a module

---
+++ estimatingANPP.R
@@ -0,0 +1,196 @@
+
+# Everything in this file gets sourced during simInit, and all functions and objects
+# are put into the simList. To use objects, use sim$xxx, and are thus globally available
+# to all modules. Functions can be used without sim$ as they are namespaced, like functions
+# in R packages. If exact location is required, functions will be: sim$<moduleName>$FunctionName
+defineModule(sim, list(
+  name = "estimatingANPP",
+  description = NA, #"insert module description here",
+  keywords = NA, # c("insert key words here"),
+  authors = c(person(c("Ian"), "Eddy", email = "ian.eddy@example.com", role = c("aut", "cre"))),
+  childModules = character(0),
+  version = list(SpaDES.core = "0.2.6", estimatingANPP = "0.0.1"),
+  spatialExtent = raster::extent(rep(NA_real_, 4)),
+  timeframe = as.POSIXlt(c(NA, NA)),
+  timeunit = "year",
+  citation = list("citation.bib"),
+  documentation = list("README.txt", "estimatingANPP.Rmd"),
+  reqdPkgs = list(),
+  parameters = rbind(
+    #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
+    defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
+    defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between plot events"),
+    defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
+    defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between save events"),
+    defineParameter(".useCache", "logical", FALSE, NA, NA, paste("Should this entire module be run with caching activated?",
+    "This is generally intended for data-type modules, where stochasticity and time are not relevant"))
+  ),
+  inputObjects = bind_rows(
+    #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
+    expectsInput(objectName = "factorialSpeciesTable", objectClass = "data.table", 
+                 desc = paste("table with species traits for matching to factorialCohortData"),
+                 sourceURL = "https://drive.google.com/open?id=1u0aXpJKbFYisEPjczTHYfXu6xZjz9O_8"),
+    expectsInput(objectName = "reducedFactorialCohortData", objectClass = "data.table", 
+                 desc = paste("results of factorial species trait simulation. This can be found by running",
+                              "the script in landRFactorial.R but you need a special LBMR"), 
+                 sourceURL = "https://drive.google.com/open?id=1rv_ROwBhCmu4K6ZiicsphjKgsHthrmBn"),
+    expectsInput(objectName = "PSPmeasure", objectClass = "data.table", desc = "merged PSP and TSP individual measurements"),
+    expectsInput(objectName = "PSPplot", objectClass = "data.table", desc = "merged PSP and TSP plot data"),
+    expectsInput(objectName = "PSPgis", objectClass = "sf", desc = "Plot location sf object. Contains duplicates"),
+    expectsInput("species", "data.table",
+                 desc = paste("a table that has species traits such as longevity, shade tolerance, etc.",
+                              "Default is partially based on Dominic Cir and Yan's project"),
+                 sourceURL = "https://raw.githubusercontent.com/dcyr/LANDIS-II_IA_generalUseFiles/master/speciesTraits.csv"),
+    expectsInput("speciesEcoregion", "data.table",
+                 desc = paste("table defining the maxANPP, maxB and SEP, which can change with both ecoregion and simulation time.",
+                              "Defaults to a dummy table based on dummy data os biomass, age, ecoregion and land cover class")),
+    expectsInput("studyAreaANPP", "SpatialPolygonsDataFrame", desc = "study area used to crop PSP data before building growth curves")
+  ),
+  outputObjects = bind_rows(
+    #createsOutput("objectName", "objectClass", "output object description", ...),
+    createsOutput(objectName = NA, objectClass = NA, desc = NA)
+  )
+))
+
+## event types
+#   - type `init` is required for initialization
+
+doEvent.estimatingANPP = function(sim, eventTime, eventType) {
+  switch(
+    eventType,
+    init = {
+      ### check for more detailed object dependencies:
+      ### (use `checkObject` or similar)
+
+      # do stuff for this event
+      sim <- Init(sim)
+
+      # schedule future event(s)
+      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "estimatingANPP", "plot")
+      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "estimatingANPP", "save")
+    },
+    plot = {
+      # ! ----- EDIT BELOW ----- ! #
+      # do stuff for this event
+
+      #plotFun(sim) # uncomment this, replace with object to plot
+      # schedule future event(s)
+
+      # e.g.,
+      #sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "estimatingANPP", "plot")
+
+      # ! ----- STOP EDITING ----- ! #
+    },
+    save = {
+      # ! ----- EDIT BELOW ----- ! #
+      # do stuff for this event
+
+      # e.g., call your custom functions/methods here
+      # you can define your own methods below this `doEvent` function
+
+      # schedule future event(s)
+
+      # e.g.,
+      # sim <- scheduleEvent(sim, time(sim) + P(sim)$.saveInterval, "estimatingANPP", "save")
+
+      # ! ----- STOP EDITING ----- ! #
+    },
+    event1 = {
+      # ! ----- EDIT BELOW ----- ! #
+      # do stuff for this event
+
+      # e.g., call your custom functions/methods here
+      # you can define your own methods below this `doEvent` function
+
+      # schedule future event(s)
+
+      # e.g.,
+      # sim <- scheduleEvent(sim, time(sim) + increment, "estimatingANPP", "templateEvent")
+
+      # ! ----- STOP EDITING ----- ! #
+    },
+
+    warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
+                  "' in module '", current(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
+  )
+  return(invisible(sim))
+}
+
+## event functions
+#   - keep event functions short and clean, modularize by calling subroutines from section below.
+
+### template initialization
+Init <- function(sim) {
+  # # ! ----- EDIT BELOW ----- ! #
+#this module needs to clean and treat the PSP data, 
+  # ! ----- STOP EDITING ----- ! #
+
+  return(invisible(sim))
+}
+
+### template for save events
+Save <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # do stuff for this event
+  sim <- saveFiles(sim)
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+### template for plot events
+plotFun <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # do stuff for this event
+  #Plot(sim$object)
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+### template for your event1
+Event1 <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
+  # sim$event1Test1 <- " this is test for event 1. " # for dummy unit test
+  # sim$event1Test2 <- 999 # for dummy unit test
+
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+.inputObjects <- function(sim) {
+  # Any code written here will be run during the simInit for the purpose of creating
+  # any objects required by this module and identified in the inputObjects element of defineModule.
+  # This is useful if there is something required before simulation to produce the module
+  # object dependencies, including such things as downloading default datasets, e.g.,
+  # downloadData("LCC2005", modulePath(sim)).
+  # Nothing should be created here that does not create a named object in inputObjects.
+  # Any other initiation procedures should be put in "init" eventType of the doEvent function.
+  # Note: the module developer can check if an object is 'suppliedElsewhere' to
+  # selectively skip unnecessary steps because the user has provided those inputObjects in the
+  # simInit call, or another module will supply or has supplied it. e.g.,
+  # if (!suppliedElsewhere('defaultColor', sim)) {
+  #   sim$map <- Cache(prepInputs, extractURL('map')) # download, extract, load file from url in sourceURL
+  # }
+
+  #cacheTags <- c(currentModule(sim), "function:.inputObjects") ## uncomment this if Cache is being used
+  dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
+  message(currentModule(sim), ": using dataPath '", dPath, "'.")
+
+  if (!suppliedElsewhere("reducedFactorialCohortData", sim)) {
+    sim$reducedFactorialCohortData <- prepInputs(targetFile = "reducedFactorialCD.Rdat", 
+                                                 url = extractURL('reducedFactorialCohortData', sim),
+                                                 useCache = TRUE)
+  }
+  
+  if (!suppliedElsewhere("factorialSpeciesTable", sim)) {
+    sim$factorialSpeciesTable <- prepInputs(targetFile = "factorialSpeciesTable.Rdat",
+                                            url = extractURL('factorialSpeciesTable', sim), 
+                                            useCache = TRUE)
+  }
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+### add additional events as needed by copy/pasting from above
